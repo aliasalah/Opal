26ce80fee29cd32c675d597d1cb18be4d2e8a167
omar <timonquint@yahoo.com>
2019-02-13 00:28:14 +0200

    AddingCartInNewBranch

--- The Local Souq/Category.js
+++ The Local Souq/Category.js
@@ -1,4 +1,25 @@
+//by Omar declare some variables that will be used in cart handling
+var noOfItemsInCart = 0;
+var arrOfCartItems = [];
+var cartItem;
+///////////////
+
+    
+
 $( document ).ready(function() {
+    
+//by Omar update cart no of items
+var cartNo = document.querySelector("#cartNo"); 
+if (sessionStorage.getItem("noOfItemsInCart")!=null){
+    noOfItemsInCart=sessionStorage.getItem("noOfItemsInCart");
+    cartNo.innerText = noOfItemsInCart;
+}
+if (noOfItemsInCart > 0){
+    var tmpStr = localStorage.getItem("arrOfCartItems");
+    arrOfCartItems = JSON.parse(tmpStr);
+}
+///////////////
+    
     // Run code
 
 $("#nav li a").on("click", function(){
@@ -22,11 +43,60 @@ function  ReadJson(path) {
 path=sessionStorage.getItem('path');
 sessionStorage.clear();
 val=ReadJson(path);
-$('#P1').attr('src', val["Products"][7].images[0]);
+$('#P1').attr('src', val["Products"][0].images[0]);
 $('#P2').attr('src', val["Products"][1].images[0]);
 $('#P3').attr('src', val["Products"][2].images[0]);
 $('#P4').attr('src', val["Products"][3].images[0]);
 $('#P5').attr('src', val["Products"][4].images[0]);
 $('#P6').attr('src', val["Products"][5].images[0]);
 $('#P7').attr('src', val["Products"][6].images[0]);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+//Cart Handling // classes and values added to buttons //values and classes were added in html to each "+" or "-" button // the values represent the item index in the JSON Array  
+    
+ 
+var btnsAddArr = document.querySelectorAll("button.AddToCartBtn");
+btnsAddArr.forEach(function(clkdBtn){
+    clkdBtn.addEventListener("click",function(){
+        noOfItemsInCart ++;
+        cartNo.innerText = noOfItemsInCart;
+        sessionStorage.setItem("noOfItemsInCart",noOfItemsInCart);
+        cartItem = val["Products"][parseInt(clkdBtn.value)];
+        if (arrOfCartItems.indexOf(cartItem) === -1){ //if that item wan not added before push it in the array
+            cartItem["count"]=1; 
+            arrOfCartItems.push(cartItem);
+            }
+        else
+            arrOfCartItems[arrOfCartItems.indexOf(cartItem)]["count"] +=1;
+    });
 });
+    
+var btnsRmvArr = document.querySelectorAll("button.RmvFrmCartBtn");
+btnsRmvArr.forEach(function(clkdBtn){
+    clkdBtn.addEventListener("click",function(){
+        cartItemIndex = arrOfCartItems.indexOf(cartItem);
+        cartItem = val["Products"][parseInt(clkdBtn.value)];
+        if (cartItemIndex != -1){            //if that item was added before
+            noOfItemsInCart --;
+            cartNo.innerText = noOfItemsInCart;
+            sessionStorage.setItem("noOfItemsInCart",noOfItemsInCart);
+            if(arrOfCartItems[cartItemIndex].count != 1)
+                arrOfCartItems[cartItemIndex].count -=1;
+            else
+                arrOfCartItems.splice(cartItemIndex,1);
+        }
+        
+    });
+});
+
+});
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//saving the cart items to session storage before leavong the page
+
+window.onunload = function() {
+    localStorage.setItem("arrOfCartItems", JSON.stringify(arrOfCartItems))
+};
+
+
+
---
+++ The Local Souq/reviewYourCart.js
@@ -0,0 +1,70 @@
+var noOfItemsInCart = 0;
+var arrOfCartItems = [];
+
+onload=function(){    
+var cartContent = document.querySelector("#cartContent");
+    
+var cartNo = document.querySelector("#cartNo"); 
+if (sessionStorage.getItem("noOfItemsInCart")!=null){
+    noOfItemsInCart=sessionStorage.getItem("noOfItemsInCart");
+    cartNo.innerText = noOfItemsInCart;
+}
+    
+if (noOfItemsInCart == 0){
+    cartContent.innerText = "You have no items in your cart yet!";
+}
+else if (noOfItemsInCart > 0){
+    var tmpStr = localStorage.getItem("arrOfCartItems");
+    arrOfCartItems = JSON.parse(tmpStr);
+    
+    for(var i=0; i<arrOfCartItems.length; i++){
+        displayCartItem(arrOfCartItems[i]);
+        
+    }
+    
+}
+    
+    
+}; //onload
+
+
+function displayCartItem(curItem){
+    let tmpDivItem = document.createElement("div");
+    let tmpTbl = document.createElement("table");
+    tmpDivItem.appendChild(tmpTbl);
+    let tmpR1 = document.createElement("tr");
+    tmpTbl.appendChild(tmpR1);
+    let tmpR1C1 = document.createElement("td");
+    tmpR1C1.colSpan="3";
+    tmpR1.appendChild(tmpR1C1);
+    tmpR1C1.innerHTML = curItem["Title"]+'<span class="material-icons rightAligned">cancel</span>';
+    
+    let tmpR2 = document.createElement("tr");
+    tmpTbl.appendChild(tmpR2);
+    let tmpR2C1 = document.createElement("td");
+    tmpR2C1.colSpan="3";
+    tmpR2.appendChild(tmpR2C1);
+    tmpR2C1.innerHTML='<img src="'+curItem["images"][0]+'">';
+    
+    let tmpR3 = document.createElement("tr");
+    tmpTbl.appendChild(tmpR3);
+    let tmpR3C1 = document.createElement("td");
+    tmpR3C1.colSpan="3";
+    tmpR3.appendChild(tmpR3C1);
+    tmpR3C1.innerText = curItem["price"]+"EGP";
+    
+    let tmpR4 = document.createElement("tr");
+    tmpTbl.appendChild(tmpR4);
+    let tmpR4C1 = document.createElement("td");
+    let tmpR4C2 = document.createElement("td");
+    let tmpR4C3 = document.createElement("td");
+    tmpR4.appendChild(tmpR4C1);
+    tmpR4.appendChild(tmpR4C2);
+    tmpR4.appendChild(tmpR4C3);
+    tmpR4C1.innerHTML='<span class="material-icons">add_circle</span>';
+    tmpR4C2.innerText = "Count : " + curItem["count"];
+    tmpR4C3.innerHTML='<span class="material-icons">remove_circle</span>';
+    
+    cartContent.appendChild(tmpDivItem);
+};
+
